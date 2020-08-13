4ea4900504d7d854de0a9b39e3c7384dbc613a5f
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-13 19:54:22 -0400

    add rough UserCard component

--- github-explorer/components/UserCard.js
+++ github-explorer/components/UserCard.js
@@ -1,12 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
+import ExtLink from "./library/ExtLink";
+
 const UserCard = ({ user }) => {
   return (
     <CardContainer>
       <Card>
         <Avatar src={user.avatar_url}></Avatar>
-        <Name>{user.name}</Name>
+        <Nametag>
+          <Name>{user.name}</Name>
+          <Username>{`(${user.login})`}</Username>
+        </Nametag>
+        <Block>
+          <ExtLink href={user.blog}>website</ExtLink>
+          <ExtLink href={`https://twitter.com/${user.twitter_username}`}>
+            {`@${user.twitter_username}`}
+          </ExtLink>
+        </Block>
+        <Block>
+          <Text>{`${user.followers} followers`}</Text>
+          <Text>{`${user.following} following`}</Text>
+        </Block>
+        <Block>
+          <Text>{`${user.public_repos} public repos`}</Text>
+        </Block>
       </Card>
     </CardContainer>
   );
@@ -15,15 +33,43 @@ const UserCard = ({ user }) => {
 export default UserCard;
 
 const CardContainer = styled.div`
+  padding: 3rem;
+`;
+
+const Card = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 1rem;
+  align-items: baseline;
 `;
-
-const Card = styled.div``;
 
 const Avatar = styled.img`
   border-radius: 50%;
+  grid-column: span 4;
+`;
+
+const Nametag = styled.div`
+  display: flex;
+  align-items: baseline;
 `;
 
 const Name = styled.h2``;
+
+const Username = styled.h4`
+  margin-left: 0.5rem;
+  color: #777;
+`;
+
+const Block = styled.div`
+  display: flex;
+
+  a:not(:last-of-type) {
+    margin-right: 1rem;
+  }
+`;
+
+const Text = styled.p`
+  :not(:last-child) {
+    margin-right: 1rem;
+  }
+`;
