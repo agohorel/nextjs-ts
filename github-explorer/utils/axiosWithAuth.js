415b16aa3455c1c88726b8450e4ea87f82cda609
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-14 17:50:07 -0400

    fix token formatting

--- github-explorer/utils/axiosWithAuth.js
+++ github-explorer/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ import config from "../config";
 const axiosWithAuth = () => {
   return axios.create({
     headers: {
-      authorization: config.github_token,
+      authorization: `Bearer ${config.github_token}`,
     },
     baseURL: "https://api.github.com/",
   });
