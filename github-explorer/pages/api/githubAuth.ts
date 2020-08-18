247c11b1452323833a0f2976cf90109f81f9373e
agohorel <3317203+agohorel@users.noreply.github.com>
2020-08-18 15:16:46 -0400

    add API route for fetching github token

---
+++ github-explorer/pages/api/githubAuth.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import config from '../../config';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ github_token: config.github_token }));
+};
