0052132d6baec08fc5bc035abdcbe5f23ab862fb
agohorel <3317203+agohorel@users.noreply.github.com>
2020-08-18 23:34:44 -0400

    draw token from .env

--- github-explorer/pages/api/githubAuth.ts
+++ github-explorer/pages/api/githubAuth.ts
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import config from '../../token';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ github_token: config.github_token }));
+  res.end(JSON.stringify({ github_token: process.env.GITHUB_TOKEN }));
 };
